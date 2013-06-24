2cc35dde83db1b5756f56c21f3901a5df76a8a5a
kensuke <k-sato@shuchi-planning.co.jp>
2013-06-24 15:17:32 +0900

    patch maguro ACodec.cpp

--- frameworks/av/media/libstagefright/ACodec.cpp
+++ frameworks/av/media/libstagefright/ACodec.cpp
@@ -1869,7 +1869,8 @@ status_t ACodec::setupAVCEncoderParameters(const sp<AMessage> &msg) {
         if (err != OK) {
             ALOGE("Setting intra macroblock refresh mode (%d) failed: 0x%x",
                     err, intraRefreshMode);
-            return err;
+            ALOGE("setupAVCEncoderParameters() SKIP!! intra-refresh-mode");
+            //return err;
         }
     }
 
