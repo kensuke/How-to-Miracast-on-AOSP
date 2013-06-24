407f17abe7daf76cb62968ed5f5470c767089fda
kensuke <k-sato@shuchi-planning.co.jp>
2013-06-24 12:54:11 +0900

    maguro default ACodec.cpp

---
+++ frameworks/av/media/libstagefright/Android.mk
@@ -0,0 +1,124 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+include frameworks/av/media/libstagefright/codecs/common/Config.mk
+
+LOCAL_SRC_FILES:=                         \
+        ACodec.cpp                        \
+        AACExtractor.cpp                  \
+        AACWriter.cpp                     \
+        AMRExtractor.cpp                  \
+        AMRWriter.cpp                     \
+        AudioPlayer.cpp                   \
+        AudioSource.cpp                   \
+        AwesomePlayer.cpp                 \
+        CameraSource.cpp                  \
+        CameraSourceTimeLapse.cpp         \
+        DataSource.cpp                    \
+        DRMExtractor.cpp                  \
+        ESDS.cpp                          \
+        FileSource.cpp                    \
+        FLACExtractor.cpp                 \
+        FragmentedMP4Extractor.cpp        \
+        HTTPBase.cpp                      \
+        JPEGSource.cpp                    \
+        MP3Extractor.cpp                  \
+        MPEG2TSWriter.cpp                 \
+        MPEG4Extractor.cpp                \
+        MPEG4Writer.cpp                   \
+        MediaBuffer.cpp                   \
+        MediaBufferGroup.cpp              \
+        MediaCodec.cpp                    \
+        MediaCodecList.cpp                \
+        MediaDefs.cpp                     \
+        MediaExtractor.cpp                \
+        MediaSource.cpp                   \
+        MetaData.cpp                      \
+        NuCachedSource2.cpp               \
+        NuMediaExtractor.cpp              \
+        OMXClient.cpp                     \
+        OMXCodec.cpp                      \
+        OggExtractor.cpp                  \
+        SampleIterator.cpp                \
+        SampleTable.cpp                   \
+        SkipCutBuffer.cpp                 \
+        StagefrightMediaScanner.cpp       \
+        StagefrightMetadataRetriever.cpp  \
+        SurfaceMediaSource.cpp            \
+        ThrottledSource.cpp               \
+        TimeSource.cpp                    \
+        TimedEventQueue.cpp               \
+        Utils.cpp                         \
+        VBRISeeker.cpp                    \
+        WAVExtractor.cpp                  \
+        WVMExtractor.cpp                  \
+        XINGSeeker.cpp                    \
+        avc_utils.cpp                     \
+        mp4/FragmentedMP4Parser.cpp       \
+        mp4/TrackFragment.cpp             \
+
+LOCAL_C_INCLUDES:= \
+        $(TOP)/frameworks/av/include/media/stagefright/timedtext \
+        $(TOP)/frameworks/native/include/media/hardware \
+        $(TOP)/frameworks/native/include/media/openmax \
+        $(TOP)/external/flac/include \
+        $(TOP)/external/tremolo \
+        $(TOP)/external/openssl/include \
+
+LOCAL_SHARED_LIBRARIES := \
+        libbinder \
+        libcamera_client \
+        libcrypto \
+        libcutils \
+        libdl \
+        libdrmframework \
+        libexpat \
+        libgui \
+        libicui18n \
+        libicuuc \
+        liblog \
+        libmedia \
+        libmedia_native \
+        libsonivox \
+        libssl \
+        libstagefright_omx \
+        libstagefright_yuv \
+        libsync \
+        libui \
+        libutils \
+        libvorbisidec \
+        libz \
+
+LOCAL_STATIC_LIBRARIES := \
+        libstagefright_color_conversion \
+        libstagefright_aacenc \
+        libstagefright_matroska \
+        libstagefright_timedtext \
+        libvpx \
+        libstagefright_mpeg2ts \
+        libstagefright_httplive \
+        libstagefright_id3 \
+        libFLAC \
+
+LOCAL_SRC_FILES += \
+        chromium_http_stub.cpp
+LOCAL_CPPFLAGS += -DCHROMIUM_AVAILABLE=1
+
+LOCAL_SHARED_LIBRARIES += libstlport
+include external/stlport/libstlport.mk
+
+LOCAL_SHARED_LIBRARIES += \
+        libstagefright_enc_common \
+        libstagefright_avc_common \
+        libstagefright_foundation \
+        libdl
+
+LOCAL_CFLAGS += -Wno-multichar
+
+LOCAL_MODULE:= libstagefright
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
